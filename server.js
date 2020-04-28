4024d0d61f668aacb020b9263e6135f44fd44359
Monarch Wadia <monarch@zeroprojects.ca>
2020-04-28 12:40:57 -0400

    Now its fullstack!

--- server.js
+++ server.js
@@ -7,13 +7,21 @@ const folderInWhichToLook = 'public';
 const staticMiddleware = express.static(folderInWhichToLook)
 app.use(staticMiddleware);
 
+let numberOfClicks = 0;
+
 // i changed my route because i want my `index.html` to be server from the root
 app.get('/api/json', (req, res) => {
   const json = {};
   json.message = 'Hello!! World!!!!!';
   json.otherMessage = 'Foo Bar Baz';
+  json.numberOfClicks = numberOfClicks;
 
   res.json(json);
 });
+
+app.post('/api/json', (req, res) => {
+  numberOfClicks++;
+  res.redirect('/');
+})
 
 app.listen(3000, () => console.log(`Now listening on localhost:3000`));
