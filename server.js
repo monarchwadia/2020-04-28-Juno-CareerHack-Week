2a7e6eb7ee0c054cd9eef68d0678115376007120
LucasNemeth <l_orion_n@hotmail.com>
2020-04-28 11:49:51 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => console.log(`Now listening on localhost:3000`));
